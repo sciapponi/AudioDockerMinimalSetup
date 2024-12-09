fcdc1502db3def666a7502794a5e237c29b0f2c5
Sebastian <sebastian.cavada.lab@gmail.com>
2024-12-09 11:36:41 +0100

    modified:   README.md 	modified:   conda/Dockerfile 	modified:   pytorch/Dockerfile 	modified:   run.sh 	modified:   volume/install.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define variables
-containername="vocos-experiments-sciapponi-e3da-4"
+containername="NAC2VEC-sciapponi-e3da-1"
 
 # Run docker
-docker run --name $containername -u 0 --gpus all --rm -it --shm-size=32gb -e "DOCKER_CONTAINER_NAME=$containername" -v  $PWD/..:/workspace/    vocos-experiment:1.0 bash
+docker run --name $containername -u 1003 --gpus all --rm -it --shm-size=48gb -e "DOCKER_CONTAINER_NAME=$containername" -v  /raid/home/e3da:/workspace/    vocos-experiment:1.0 bash
